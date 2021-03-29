150203a15b237c832d6ecab0f368ce47589c4458
Bradley Watton <bw@bradw.io>
2021-03-29 18:55:51 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
-export * from '@strdst/utils.binary/lib/UUID'
-export * from '@strdst/utils.binary/lib/types'
-export * from '@strdst/utils.binary/lib/vector'
+import {
+  UUID,
+  DataLengthBigInt,
+  DataLengthNumber,
+  DataLengths,
+  IAddress,
+  AddressFamily,
+  Vector2,
+  Vector3,
+  Vector4,
+  Vector5,
+} from '@strdst/utils.binary'
+
+export {
+  UUID,
+  DataLengthBigInt,
+  DataLengthNumber,
+  DataLengths,
+  IAddress,
+  AddressFamily,
+  Vector2,
+  Vector3,
+  Vector4,
+  Vector5,
+}
 
 export * from './data'
 export * from './BinaryData'
+export * from './Metadata'
 export * from './types'
